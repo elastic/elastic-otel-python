25484fa25510c5e54230d62108173dfd0efe80d8
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2024-05-24 14:55:41 +0200

    Add process runtime resource detector (#37)
    * Setup pytest
    
    * sdk: introduce ProcessRuntimeResourceDetector
    
    Add a process_runtime resource detector that only sets process runtime
    related resource attributes.
    
    * distro: enable process runtime resource detector by default
    
    * ci: run unit tests on test matrix

---
+++ tests/resources/test_resources.py
@@ -0,0 +1,35 @@
+# Copyright Elasticsearch BV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from unittest import TestCase
+
+from elasticotel.sdk.resources import ProcessRuntimeResourceDetector
+from opentelemetry.sdk.resources import (
+    PROCESS_RUNTIME_NAME,
+    PROCESS_RUNTIME_DESCRIPTION,
+    PROCESS_RUNTIME_VERSION,
+    Resource,
+    get_aggregated_resources,
+)
+
+
+class TestProcessRuntimeDetector(TestCase):
+    def test_process_runtime_detector(self):
+        initial_resource = Resource(attributes={})
+        aggregated_resource = get_aggregated_resources([ProcessRuntimeResourceDetector()], initial_resource)
+
+        self.assertEqual(
+            sorted(aggregated_resource.attributes.keys()),
+            [PROCESS_RUNTIME_DESCRIPTION, PROCESS_RUNTIME_NAME, PROCESS_RUNTIME_VERSION],
+        )
