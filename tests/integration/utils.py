06aa138dbc33d7794f34cce9d054413e34ee5cbc
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2024-11-07 11:40:29 +0100

    Bump oteltest to 0.24.0 (#170)
    Fix #74

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -137,7 +137,7 @@ class ElasticIntegrationTestCase(unittest.TestCase):
         metrics = []
         for request in telemetry["metric_requests"]:
             elems = []
-            for proto_elem in request["request"]["resourceMetrics"]:
+            for proto_elem in request["pbreq"]["resourceMetrics"]:
                 scope_metrics = []
                 for proto_scope_metric in proto_elem["scopeMetrics"]:
                     scope_metric = proto_scope_metric.copy()
@@ -157,7 +157,7 @@ class ElasticIntegrationTestCase(unittest.TestCase):
 
         traces = []
         for request in telemetry["trace_requests"]:
-            for resource_span in request["request"]["resourceSpans"]:
+            for resource_span in request["pbreq"]["resourceSpans"]:
                 resource_attributes = normalize_attributes(resource_span["resource"]["attributes"])
                 for proto_scope_spans in resource_span["scopeSpans"]:
                     for proto_span in proto_scope_spans["spans"]:
