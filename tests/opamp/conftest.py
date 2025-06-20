071c082fbbe5a9a24f41d4c5646c680532574e35
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2025-06-20 18:21:24 +0200

    Add e2e tests with registered responses

---
+++ tests/opamp/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        "filter_headers": [
+            ("authorization", "Bearer key"),
+        ],
+        "decode_compressed_response": True,
+        "before_record_response": scrub_response_headers,
+    }
+
+
+def scrub_response_headers(response):
+    """
+    This scrubs sensitive response headers. Note they are case-sensitive!
+    """
+    return response
