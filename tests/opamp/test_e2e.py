d84ef1b0238d5b9cd5aa448ebb3e5b95e67c836e
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2025-06-23 09:35:19 +0200

    Rename e2e and add tests with server not responding

--- tests/opamp/test_e2e.py
+++ tests/opamp/test_e2e.py
@@ -11,7 +11,7 @@ from opentelemetry._opamp.client import OpAMPClient
 
 @pytest.mark.skipif(sys.version_info < (3, 10), reason="vcr.py not working with urllib 2 and older Pythons")
 @pytest.mark.vcr()
-def test_e2e(caplog):
+def test_connection_heartbeat_disconnection(caplog):
     caplog.set_level(logging.DEBUG, logger="opentelemetry._opamp.agent")
 
     opamp_handler = mock.Mock()
@@ -37,3 +37,29 @@ def test_e2e(caplog):
 
     # one call is for connection and the other one is heartbeat
     assert opamp_handler.call_count == 2
+
+
+@pytest.mark.skipif(sys.version_info < (3, 10), reason="vcr.py not working with urllib 2 and older Pythons")
+@pytest.mark.vcr()
+def test_with_server_not_responding(caplog):
+    caplog.set_level(logging.DEBUG, logger="opentelemetry._opamp.agent")
+
+    opamp_handler = mock.Mock()
+
+    opamp_client = OpAMPClient(
+        endpoint="http://localhost:4320/v1/opamp",
+        agent_identifying_attributes={
+            "service.name": "foo",
+            "deployment.environment.name": "foo",
+        },
+    )
+    opamp_agent = OpAMPAgent(
+        interval=1,
+        message_handler=opamp_handler,
+        client=opamp_client,
+    )
+    opamp_agent.start()
+
+    opamp_agent.stop()
+
+    assert opamp_handler.call_count == 0
